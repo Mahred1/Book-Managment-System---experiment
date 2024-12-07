a946bcf9d3b2779ad9f24f544fc2b39bbab2692a
mredwan <mredwan.kemal@gmail.com>
2024-12-07 21:05:32 +0300

    finished adding functionality to the profile modal with JS.@Mahfuz

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,7 @@
 const books= document.querySelectorAll(".book-container");
 const filter= document.querySelector(".filter-items");
+const profile = document.querySelector(".profile");
+const profileModal= document.querySelector(".profile-modal");
 
 filter.addEventListener("click",(event)=>{
     if (!event.target.classList.contains("active") && event.target.classList.contains("filter-item")) {
@@ -20,4 +22,8 @@ filter.addEventListener("click",(event)=>{
       }
     })
 
+})
+
+profile.addEventListener("click",()=>{
+    profileModal.classList.toggle("active-modal");
 })
