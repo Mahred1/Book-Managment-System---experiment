b302bb56577b203a03669742163d81a79774dae2
mredwan <mredwan.kemal@gmail.com>
2024-12-07 17:37:08 +0300

    finished addin functinality to thee book filter with JAVASCRIPT @Mahfuz

---
+++ JS/app.js
@@ -0,0 +1,20 @@
+const books= document.querySelectorAll(".book-container");
+const filter= document.querySelector(".filter-items");
+
+filter.addEventListener("click",(event)=>{
+    if (!event.target.classList.contains("active")) {
+       document.querySelector(".active").classList.remove("active");
+       event.target.classList.add("active");
+       event.stopPropagation();
+    }
+    const filterValue= event.target.getAttribute("data");
+    books.forEach(book=>{
+        if (book.classList.contains(filterValue) || filterValue==="All") {
+            book.classList.remove("hide");
+            book.classList.add("show");
+        }else{
+            book.classList.remove("show");
+            book.classList.add("hide");
+        }
+    })
+})
